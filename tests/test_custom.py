71f47988b23239fc7abed5ad35d393424c86f8cc
Valentin Berlier <berlier.v@gmail.com>
2020-11-01 02:11:11 +0100

    Add tests

--- tests/test_custom.py
+++ tests/test_custom.py
@@ -1,10 +1,12 @@
+from dataclasses import dataclass
 from pathlib import Path
-from typing import NamedTuple
+from typing import Tuple
 
 from pytest_insta import Fmt
 
 
-class Point(NamedTuple):
+@dataclass
+class Point:
     x: int
     y: int
 
@@ -22,3 +24,19 @@ class FmtPoint(Fmt[Point]):
 def test_point(snapshot):
     assert snapshot("pt") == Point(4, 2)
     assert snapshot(".pt") == Point(4, 5)
+
+
+class FmtTextPair(Fmt[Tuple[str, str]]):
+    extension = ".textpair"
+
+    def load(self, path: Path) -> Tuple[str, str]:
+        return (path / "left.txt").read_text(), (path / "right.txt").read_text()
+
+    def dump(self, path: Path, value: Tuple[str, str]):
+        path.mkdir(exist_ok=True)
+        (path / "left.txt").write_text(value[0])
+        (path / "right.txt").write_text(value[1])
+
+
+def test_text_dir(snapshot):
+    assert snapshot("textpair") == ("hello", "world")
